5a32e30adb1ebfa37f28e2a89300619bad493af6
Alissa Troiano <alissatroianonyc@gmail.com>
2025-08-10 00:36:22 -0400

    Update layout and styles

--- trendtracker/src/components/App/App.tsx
+++ trendtracker/src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import appLogo from '/logo.png'
+import appLogo from '/trend-tracker.png'
 import './App.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -13,20 +13,33 @@ function App() {
       <Container>
         <Row>
           <Col>
-          <div className="img-wrapper">
+            <div className="img-wrapper">
               <img src={appLogo} className="logo" alt="trend-tracker" />
-          </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h1>Trend Tracker</h1>
-          </Col>
-          <Col>
-            <div className="card">
-             
             </div>
           </Col>
+        </Row>
+        <Row className='d-flex flex-row justify-content-center'>
+          <Col className='col-12 col-sm-6 flex-col'>
+            <div className='card'>
+              <div className='card-header'>
+                <h1 className='display-3'>New Users</h1>
+              </div>
+              <div className='card-body'>
+                <p >This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+              </div>
+            </div>
+          </Col>
+          <Col className='col-12 col-sm-6 flex-col'>
+            <div className='card'>
+              <div className='card-header'>
+                <h1 className='display-3'>New Users</h1>
+              </div>
+              <div className='card-body'>
+                           <p >This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+              </div>
+            </div>
+          </Col>
+
         </Row>
       </Container>
     </>
